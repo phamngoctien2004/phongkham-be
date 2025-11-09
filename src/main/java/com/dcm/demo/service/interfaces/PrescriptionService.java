721ca79326963c42edc1bc7caca9c9c2c5ffe8b1
tien dev <tienhvnhgpt@gmail.com>
2025-11-09 20:05:13 +0700

    update name db

--- src/main/java/com/dcm/demo/service/interfaces/PrescriptionService.java
+++ src/main/java/com/dcm/demo/service/interfaces/PrescriptionService.java
@@ -5,15 +5,16 @@ import com.dcm.demo.dto.request.PrescriptionRequest;
 import com.dcm.demo.dto.response.PreDetailResponse;
 import com.dcm.demo.dto.response.PrescriptionResponse;
 
-import java.util.List;
-
 public interface PrescriptionService {
     PrescriptionResponse getPrescriptionsByMedicalRecordId(Integer medicalRecordId);
+
     PrescriptionResponse create(PrescriptionRequest request);
+
     PrescriptionResponse update(PrescriptionRequest request);
 
-
     PreDetailResponse createPreDetail(PresDetailRequest request);
+
     PreDetailResponse updatePreDetail(PresDetailRequest request);
+
     void deletePreDetail(Integer id);
 }
