c4e28f9083dcb72e0bd814f098af087eac7bc7c1
tien dev <tienhvnhgpt@gmail.com>
2025-11-21 16:10:48 +0700

    test cronjob

--- src/main/java/com/dcm/demo/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/dcm/demo/service/impl/AppointmentServiceImpl.java
@@ -247,7 +247,7 @@ public class AppointmentServiceImpl implements AppointmentService {
         return false;
     }
 
-    @Scheduled(cron = "0 0 8 * * *", zone = "Asia/Ho_Chi_Minh")
+    @Scheduled(cron = "0 30 16 * * *", zone = "Asia/Ho_Chi_Minh")
     public void sendReminderAppointments() {
         LocalDate tomorrow = LocalDate.now().plusDays(1);
         List<Appointment> appointments = repository.findByDate(tomorrow);
